772c162cc8d6c5ac669a99f47362c5ac1d9fe03f
Prince Donda <princedonda4489@gmail.com>
2026-05-12 22:30:38 +0530

    Add initial HTML, CSS, and JavaScript files for Travel Info website

---
+++ script.js
@@ -0,0 +1,11 @@
+// Future JavaScript for interactivity can be added here.
+
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
